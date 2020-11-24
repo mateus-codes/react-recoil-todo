194d3373252c72e75da979bcbf4d200555aaa977
mateus-codes <mateus.automation@gmail.com>
2020-11-24 11:06:57 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { RecoilRoot } from 'recoil';
+import TodoList from './TodoList'
+
+function App() {
+  return (
+    <RecoilRoot>
+      <TodoList />
+    </RecoilRoot>
+  );
+}
+
+export default App;
